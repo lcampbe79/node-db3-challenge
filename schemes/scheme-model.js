dd9e27f0e49961636222e67bd04d81b9a3b2bf19
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-06 14:43:14 -0800

    Able to findSteps().

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,15 +2,24 @@ const db = require('../data/db-config');
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps
 }
 
 function find() {
-  return db.select('*').from('schemes')
+  return db('schemes')
 }
 
 function findById(id) {
   return db('schemes')
   .where({id})
   .first()
+}
+
+function findSteps(id) {
+  return db('schemes')
+    .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+    .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where({scheme_id : id})
+    .orderBy('steps.step_number')
 }
