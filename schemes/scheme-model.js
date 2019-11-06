d393ec029162afd3b2e27fc8b2e754b99b38d1b7
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-06 13:47:26 -0800

    findById() working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,16 @@
 const db = require('../data/db-config');
 
 module.exports = {
-  find
+  find,
+  findById
 }
 
 function find() {
   return db.select('*').from('schemes')
+}
+
+function findById(id) {
+  return db('schemes')
+  .where({id})
+  .first()
 }
